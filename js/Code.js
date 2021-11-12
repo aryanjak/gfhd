452f3fde453903229ce93827f931c2ad13eb58ef
aryanjak <94070591+aryanjak@users.noreply.github.com>
2021-11-12 20:34:42 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 700,150)
+    fill("lightblue")
+    text("Hint: Some thing not exicuted until u call it!!", 700,170)
+
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 200,350)
+    fill("lightblue")
+    text("Hint: Stores your data!!", 200,370)
+
+}
